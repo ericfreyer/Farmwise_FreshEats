b56b32340d572514f74d154b68547a0e5eb2b41c
Eric Freyer <ericcfreyer@gmail.com>
2020-12-15 20:28:54 -0500

    esiena/charlie work

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
  function GetMap(){
      var map = new Microsoft.Maps.Map('#myMap',{
          credentials: "AhmEVg3cDIQvEx4OvVH3WN-1ioOTBoC4176hphjoyr8OlMHoPDHNfmIq8VQLVGbO",
-         center: new Microsoft.Maps.Location(40.7128, 74.0060)
+         center: new Microsoft.Maps.Location(40.7128, 74.0060),
+         mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+         zoom: 10
+
      })
      
  }
