1cf22158a67de15cb774e6a592a6a61bbdbb7150
Eric Freyer <ericcfreyer@gmail.com>
2020-12-15 21:37:51 -0500

    made ajax usda

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-
-
-
-
-
-
-
-
-
-
-
-//usad AJAX
-
-var queryURL = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip)
+var zipcode = 11385
+var queryURL = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zipcode)
 
 
 $.ajax({
@@ -19,4 +7,17 @@ $.ajax({
     url: queryURL
 }).then(function(response) {
     console.log(response)
+
+id = (response.results[0].id)
+
+var queryURL2 = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id)
+
+$.ajax({
+    type: "GET",
+    url: queryURL2
+}).then(function(data) {
+    console.log(data)
 })
+})
+
+
