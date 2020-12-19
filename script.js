776f086286c3bafb309f05a490e83e8767ba4547
Eric Freyer <ericcfreyer@gmail.com>
2020-12-19 10:46:47 -0500

    added some starter code on click

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-var zipcode = 11385
-var queryURL = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zipcode)
+$(document).ready(function () {  
+
+    $("#search-this").on("click", function(event){
+    event.preventDefult()
+
+    var zipcode = $(".search-form").val().trim()
+    var queryURL = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zipcode)
 
 
-$.ajax({
-    type: "GET",
-    url: queryURL
-}).then(function(response) {
-    console.log(response)
+    $.ajax({
+        type: "GET",
+        url: queryURL
+    }).then(function(response) {
+        console.log(response)
 
-id = (response.results[0].id)
+    id = (response.results[0].id)
 
-var queryURL2 = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id)
+    var queryURL2 = ("http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id)
 
-$.ajax({
-    type: "GET",
-    url: queryURL2
-}).then(function(data) {
-    console.log(data)
+    $.ajax({
+        type: "GET",
+        url: queryURL2
+    }).then(function(data) {
+        console.log(data)
+    })
+    })
+
+}) 
 })
-})
-
-
